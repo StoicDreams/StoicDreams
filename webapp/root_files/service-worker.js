d8a957d8f693b5224c0619bef739a0d5d6c3a2d3
Erik Gassler <erik@gassler.us>
2025-07-08 07:19:59 -0500

    fix json

--- webapp/root_files/service-worker.js
+++ webapp/root_files/service-worker.js
@@ -12,7 +12,7 @@ function get_uuid() {
 	}
 }
 const cacheNamePrefix = 'offline-cache';
-const cachePostfix = location.host.startsWith('localhost') ? `_${get_uuid()}` : '_ts_2507032132';
+const cachePostfix = location.host.startsWith('localhost') ? `_${get_uuid()}` : '_ts_2507081219';
 const cacheName = `${cacheNamePrefix}${cachePostfix}`;
 const offlineAssetsInclude = [/\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.ico$/];
 const offlineAssetsExclude = [/^service-worker\.js$/];
